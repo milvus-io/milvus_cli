d7afe52cb086d2e97ac7ce7130b1cc7af9f6528c
czhen <zhen.chen@zilliz.com>
2021-08-26 17:07:25 +0800

    support rc4

--- README.md
+++ README.md
@@ -46,7 +46,7 @@ Milvus CLI based on [Milvus Python ORM SDK](https://github.com/milvus-io/pymilvu
 | 1.1.* | 1.1.2 | x |
 | 2.0.0-RC1 | 2.0.0rc1 | x |
 | 2.0.0-RC2 | 2.0.0rc2 | 0.1.3 |
-| 2.0.0-RC4 | 2.0.0rc4 |  |
+| 2.0.0-RC4 | 2.0.0rc4 | 0.1.4 |
 
 ## Installation
 
--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='milvus_cli',
-    version='0.1.3',
+    version='0.1.4',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
         'Click',
-        'pymilvus-orm==2.0.0rc2',
+        'pymilvus-orm==2.0.0rc4',
         'tabulate'
     ],
     entry_points={
